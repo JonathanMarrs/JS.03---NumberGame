52bbda28a277d6ce3d0d68985337e08f1a0bed2e
JM_Laptop <marrs.jonathan@gmail.com>
2016-09-08 21:20:41 -0700

    added boolean operator to code

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+var correctGuess = false;
 var randomNumber = Math.floor(Math.random() * 6 ) + 1;
 var guess = prompt("I'm thinking of a number between 1 and 6.  What is it?");
 
 if (parseInt(guess) === randomNumber ) {
-  document.write("<p>You guessed right!</p>")
+  correctGuess = true;
+}
+
+if ( correctGuess ) {
+ document.write("<p>You guessed right!</p>")
 } else {
-  document.write("<p>Nope. You are straight wrong! The number was " + randomNumber + ".</p>")
+  document.write("<p>Nope. You are straight wrong! The number was " +  randomNumber + ".</p>")
 }
